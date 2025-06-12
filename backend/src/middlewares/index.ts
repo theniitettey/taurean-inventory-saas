a4aad7fb7f9b08b3a74def586733bd49e0c5a9ee
michaelperryjnr <michaelperryt97@gmail.com>
2025-06-12 14:33:16 +0000

    Default export

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -1,3 +1,14 @@
 import { authenticateUser, authorizeRoles } from "./auth.middleware";
+import { LoggerMiddleware, logger } from "./logger.middleware";
+import ErrorMiddleware from "./error.middleware";
+import { APIRateLimiter, AuthRateLimiter } from "./rateLimiter.middleware";
 
-export { authenticateUser, authorizeRoles };
+export {
+  authenticateUser,
+  authorizeRoles,
+  LoggerMiddleware,
+  logger,
+  ErrorMiddleware,
+  APIRateLimiter,
+  AuthRateLimiter,
+};
