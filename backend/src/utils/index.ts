6c274db5004010af04a509ec6461b1d26c88eabc
michaelperryjnr <michaelperryt97@gmail.com>
2025-06-12 14:31:00 +0000

    Default export

---
+++ backend/src/utils/index.ts
@@ -0,0 +1,21 @@
+import {
+  sendSuccess,
+  sendError,
+  sendValidationError,
+  sendNotFound,
+  sendUnauthorized,
+  sendForbidden,
+  sendConflict,
+} from "./response.utils";
+import Logger from "./logger.utils";
+
+export {
+  sendSuccess,
+  sendError,
+  sendValidationError,
+  sendNotFound,
+  sendUnauthorized,
+  sendForbidden,
+  sendConflict,
+  Logger,
+};
