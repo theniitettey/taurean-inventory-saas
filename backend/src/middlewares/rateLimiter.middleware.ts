b122263d739d6fae3463f1baa8ce7b270e4b7311
michaelperryjnr <michaelperryt97@gmail.com>
2025-06-12 14:33:05 +0000

    Created rate limiting middlewares

---
+++ backend/src/middlewares/rateLimiter.middleware.ts
@@ -0,0 +1,27 @@
+import rateLimit from "express-rate-limit";
+
+const APIRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many requests, please try again later.",
+    statusCode: 429,
+  },
+});
+
+const AuthRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many authentication attempts, please try again later.",
+    statusCode: 429,
+  },
+});
+
+export { APIRateLimiter, AuthRateLimiter };
