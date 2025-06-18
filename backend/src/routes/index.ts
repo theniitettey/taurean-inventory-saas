987df47f07881bb8b60a3299597de0d74295a40f
michaelperryjnr <michaelperryt97@gmail.com>
2025-06-18 00:00:40 +0000

    Default export

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,4 +1,6 @@
 import userRoutes from "./user.route";
 import authRoutes from "./auth.route";
+import facilityRoutes from "./facility.route";
+import inventoryItemRoutes from "./inventoryItem.route";
 
-export { userRoutes, authRoutes };
+export { userRoutes, authRoutes, facilityRoutes, inventoryItemRoutes };
