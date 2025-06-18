f757d57aab2a2aceb1fe1012e5dcbb8d7b777ad5
michaelperryjnr <michaelperryt97@gmail.com>
2025-06-18 02:48:51 +0000

    Created transaction controller

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -2,3 +2,4 @@ export * as UserController from "./user.controller";
 export * as AuthController from "./auth.controller";
 export * as FacilityController from "./facility.controller";
 export * as InventoryItemController from "./inventoryItem.controller";
+export * as TransactionController from "./transaction.controller";
