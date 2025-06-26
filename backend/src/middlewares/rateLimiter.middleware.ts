2a1f0e6f4ff5226d8ec8ed0fedc0277c046f35af
michaelperryjnr <michaelperryt97@gmail.com>
2025-06-26 02:36:45 +0000

    Increased rate limit

--- backend/src/middlewares/rateLimiter.middleware.ts
+++ backend/src/middlewares/rateLimiter.middleware.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const APIRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 300,
   standardHeaders: true,
   legacyHeaders: false,
   message: {
